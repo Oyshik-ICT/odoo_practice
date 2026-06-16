fe691a2779188a1ac4790bee54d8dd6c4b3c4044
oyshik <oyshikf@gmail.com>
2026-06-16 14:29:59 +0600

    create the manifest and init file

---
+++ library_management/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Library Management",
+    "version": "19.0.1",
+    "summary": "Library management module for books and members",
+    "description": "A odoo module to manage books and members",
+    "depends": ["base"],
+    "license": "LGPL-3",
+    "sequence": -10,
+    "application": True,
+}
